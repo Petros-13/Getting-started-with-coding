77f4f062fca81ed760b62605d6d340d6a5d4d0ea
Petros-13 <petroskalkounos78@gmail.com>
2025-07-15 15:52:20 +0200

    2nd commit

---
+++ Main.2.py
@@ -0,0 +1,18 @@
+name="Petros"
+print(name," is in Greece")
+x=20
+y=12
+sum=x+y 
+print("The sum of x+y is",sum)
+x=2
+y=4
+product=x*y
+print("The product of x*y =",product)
+x=5
+y=4
+subtraction=x-y
+print("the subtraction of x-y =",subtraction)
+x=8
+y=2
+division=x/y
+print("The division between x and y is",division)
