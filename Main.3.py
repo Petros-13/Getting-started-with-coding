a16d3eed0f80500c630c2f4e0806aac07eca5adc
Petros-13 <petroskalkounos78@gmail.com>
2025-07-15 16:12:25 +0200

    3rd commit

---
+++ Main.3.py
@@ -0,0 +1,6 @@
+x=int(input("Enter x :"))
+y=int(input("Enter y :"))
+sum=x+y
+print("The summ of x+y is",sum)
+product=x*y
+print("The sum of x*y is",product)
---
+++ Main.4.py
@@ -0,0 +1,3 @@
+import keyword 
+print("All kewords in python are the following.\n")
+print(keyword.kwlist)
