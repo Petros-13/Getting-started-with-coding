6ae6ba610c68db2ff25ada0d025b9204cf2c2608
Petros-13 <petros.kalkounos78@gmail.com>
2025-08-14 10:40:50 +0200

    2nd commit
    2nd commit of 3rd project (nested loops)

--- Main.3.py
+++ Main.3.py
@@ -1,6 +1,24 @@
-x=int(input("Enter x :"))
-y=int(input("Enter y :"))
-sum=x+y
-print("The summ of x+y is",sum)
-product=x*y
-print("The sum of x*y is",product)
+num = int(input("Enter the number: "))
+t = num
+numLen = 0
+while t > 0:
+    numLen += 1
+    t //= 10
+
+if numLen >= 4:
+    mid_index = numLen // 2
+    chk = 0
+    temp = num
+    
+    while temp > 0:
+        rem = temp % 10
+        if chk == mid_index:
+            midOne = rem
+        elif chk == (mid_index - 1):
+            midTwo = rem
+        temp //= 10
+        chk += 1
+    prod = midOne * midTwo
+    print(f"\nProduct of Mid digits ({midOne} * {midTwo}) = {prod}")
+else:
+    print("\nIt's not a 4 or more than 4-digit number!")
